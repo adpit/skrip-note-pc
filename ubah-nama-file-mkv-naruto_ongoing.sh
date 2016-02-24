abe964cd4dc8505027a83b0202dc2d9b3c3b2219
adpit <adpit31@gmail.com>
2016-02-24 12:13:05 +0700

    ubah semua nama file mkv dengan, sesuai dengan chappternya

---
+++ ubah-nama-file-mkv-naruto_ongoing.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# format filenya download film naruto seperti ini, dengan chappter hanya ada nomor tanpa ada nama judul filmanya
+# 11.mkv
+# 20.mkv
+# 80.mkv
+# perintah ini akan merubah seluruh nama file .mkv dengan tambahkan judul yang kita inginkan dengan nomor file / chappter yang sesuai
+# for a in `ls -al | grep .mkv | awk '{print $9}' | sed 's/.mkv//'`; do mv $a "Naruto Shippuuden1 ($a).mkv";done
+
